032522545d321398f38599e3159694db18764203
dhruv narang <dhruvnarang608@gmail.com>
2026-05-01 22:58:33 +0530

    UC1: Feet measurement equality - equals() override with Double.compare()

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -1,35 +1,30 @@
 package QuantityMeasurementApp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-//@SpringBootApplication
 public class MeasurementApplication {
-	static class Feet{
-		private final double value;
 
-		public Feet(double value){
-			this.value = value;
-		}
+    static class Feet {
+        private final double value;
 
-		@Override
-		public boolean equals(Object obj){
-			if(this == obj){
-				return true;
-			}
-			if(obj == null|| getClass() != obj.getClass()){
-				return false;
-			}
-			Feet other = (Feet) obj;
-			return Double.compare(this.value, other.value) == 0;
-		}
-	}
+        public Feet(double value) {
+            this.value = value;
+        }
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeasurementApplication.class, args);
-		Feet f1 = new Feet(34.5);
-		Feet f2 = new Feet(34.5);
-		System.out.println(f1.equals(f2));
-	}
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
 
+    public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+        System.out.println("1.0 ft equals 1.0 ft: " + f1.equals(f2));
+
+        Feet f3 = new Feet(1.0);
+        Feet f4 = new Feet(2.0);
+        System.out.println("1.0 ft equals 2.0 ft: " + f3.equals(f4));
+    }
 }
