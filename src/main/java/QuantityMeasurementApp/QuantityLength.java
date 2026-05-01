9093bb8dc93a2e48a56f4c692fdfd480cff9e26b
dhruv narang <dhruvnarang608@gmail.com>
2026-05-01 23:01:35 +0530

    UC5: Unit-to-unit conversion - convertTo() method using base-unit normalisation

--- src/main/java/QuantityMeasurementApp/QuantityLength.java
+++ src/main/java/QuantityMeasurementApp/QuantityLength.java
@@ -21,6 +21,12 @@ public class QuantityLength {
         return Double.compare(this.toBaseFeet(), other.toBaseFeet()) == 0;
     }
 
+    public QuantityLength convertTo(LengthUnit target) {
+        double inFeet = toBaseFeet();
+        double converted = inFeet / target.getConversionFactor();
+        return new QuantityLength(Math.round(converted * 100.0) / 100.0, target);
+    }
+
     @Override
     public String toString() {
         return value + " " + unit;
