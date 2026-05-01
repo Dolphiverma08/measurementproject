49ef2ab7522830763a9f72a8eb32d8a559996ce4
dhruv narang <dhruvnarang608@gmail.com>
2026-05-01 22:59:17 +0530

    UC2: Inches measurement equality - separate Inches inner class alongside Feet

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -18,13 +18,29 @@ public class MeasurementApplication {
         }
     }
 
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
     public static void main(String[] args) {
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
         System.out.println("1.0 ft equals 1.0 ft: " + f1.equals(f2));
 
-        Feet f3 = new Feet(1.0);
-        Feet f4 = new Feet(2.0);
-        System.out.println("1.0 ft equals 2.0 ft: " + f3.equals(f4));
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+        System.out.println("1.0 in equals 1.0 in: " + i1.equals(i2));
     }
 }
